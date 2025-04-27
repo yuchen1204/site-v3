ea90fcdab94025a9b40694bd0c1f3f3e438b35a9
yuchen1204 <xiaoyuchen031204@gamil.com>
2025-04-28 02:21:13 +0800

    tryfix

--- scripts/blog-details.js
+++ scripts/blog-details.js
@@ -410,7 +410,7 @@ function displayComments(comments) {
         <div class="comment-item" data-comment-id="${comment.id}">
             <div class="comment-header">
                 <span class="comment-author">${escapeHTML(comment.name)}</span>
-                <span class="comment-date">${formatDate(new Date(comment.createdAt))}</span>
+                <span class="comment-date">${formatDate(new Date(comment.createdAt || comment.timestamp))}</span>
             </div>
             <div class="comment-content">${escapeHTML(comment.content)}</div>
         </div>
@@ -485,4 +485,22 @@ const sunIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" 
 const moonIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon-stars-fill" viewBox="0 0 16 16">
 <path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278"/>
 <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.73 1.73 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.73 1.73 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.73 1.73 0 0 0 1.097-1.097zM13.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.73 1.73 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162a1.73 1.73 0 0 0-1.097-1.097l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.73 1.73 0 0 0 1.097-1.097z"/>
-</svg>`; 
+</svg>`;
+
+/**
+ * 转义HTML特殊字符，防止XSS攻击
+ * @param {string} str - 要转义的字符串
+ * @returns {string} - 转义后的安全字符串
+ */
+function escapeHTML(str) {
+    if (!str) return '';
+    return str.replace(/[&<>"']/g, function(match) {
+        return {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&quot;',
+            "'": '&#39;'
+        }[match];
+    });
+} 
